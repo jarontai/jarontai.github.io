1da20dcee325a99b8bd8f6b166bb1eb9ccd602ae
daiqiu <daiqiu@appgame.com>
2014-12-11 10:44:47 +0800

    Site updated at 2014-12-11 02:44:47 UTC

---
+++ javascripts/github.js
@@ -0,0 +1,42 @@
+var filterRepoList = ['dotfiles', 'jarontai.github.io'];
+
+var github = (function(){
+  function escapeHtml(str) {
+    return $('<div/>').text(str).html();
+  }
+  function render(target, repos){
+    var i = 0, fragment = '', t = $(target)[0];
+
+    for(i = 0; i < repos.length; i++) {
+      fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+escapeHtml(repos[i].description||'')+'</p></li>';
+    }
+    t.innerHTML = fragment;
+  }
+  return {
+    showRepos: function(options){
+      $.ajax({
+          url: "https://api.github.com/users/"+options.user+"/repos?sort=pushed&callback=?"
+        , dataType: 'jsonp'
+        , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
+        , success: function(data) {
+          var repos = [];
+          if (!data || !data.data) { return; }
+          for (var i = 0; i < data.data.length; i++) {
+            if (options.skip_forks && data.data[i].fork) { continue; }
+
+            // filter repos
+            if (filterRepoList && filterRepoList.length) {
+              if (filterRepoList.indexOf(data.data[i].name) >= 0) {
+                continue;
+              }
+            }
+
+            repos.push(data.data[i]);
+          }
+          if (options.count) { repos.splice(options.count); }
+          render(options.target, repos);
+        }
+      });
+    }
+  };
+})();
